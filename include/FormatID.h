83186bb76b964db22a61dde135df357f0acd7cbd
Christoph Steup <steup@ivs.cs.ovgu.de>
2014-07-30 22:39:16 +0200

    Format moved to library

--- include/FormatID.h
+++ include/FormatID.h
@@ -4,9 +4,9 @@
 
 class FormatID{
   private:
-    static uint16_t sFormatNr;
+   static uint16_t sFormatNr;
    const uint32_t mValue;
   public:
-    FormatID(uint16_t nodeID) : mValue(((uint32_t)(nodeID)<<sizeof(nodeID)*8) | sFormatNr++){}
-    uint32_t value() const { return mValue; } 
+    FormatID(uint16_t nodeID);
+    uint32_t value() const;
 };
--- src/FormatID.cpp
+++ src/FormatID.cpp
@@ -1,4 +1,12 @@
 #include <FormatID.h>
 
+FormatID::FormatID(uint16_t nodeID) : mValue(((uint32_t)(nodeID)<<sizeof(nodeID)*8) | sFormatNr++){
+
+}
+
+uint32_t FormatID::value() const { 
+  return mValue; 
+} 
+
 uint16_t FormatID::sFormatNr = 0;
 
