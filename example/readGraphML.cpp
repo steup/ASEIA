63e46f90d603bf492a3e0ccdff7fbed685237d0d
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-06-06 04:00:18 +0200

    Working Prototype of GraphML parsing from yED:
    	- read_graphml parses transformed yED Graph
    	- Node name -> name
    	- Node shape -> type

--- example/readGraphML.cpp
+++ example/readGraphML.cpp
@@ -6,17 +6,55 @@
 using namespace boost;
 using namespace std;
 
-using Graph = adjacency_list<vecS, vecS, directedS>;
+struct NodeInfo{
+	string name, type;
+};
+
+
+using Graph = adjacency_list<vecS, vecS, directedS, NodeInfo>;
 
 const char* graphMLFile = "/home/steup/test.graphml";
+
+using VIter  = graph_traits<Graph>::vertex_iterator;
+using EIter  = graph_traits<Graph>::edge_iterator;
+using VPair  = std::pair<VIter, VIter>; 
+using EPair  = std::pair<EIter, EIter>; 
+namespace std{
+	VIter begin(VPair pair) { return pair.first; }
+	VIter end(VPair pair) { return pair.second; }
+	EIter begin(EPair pair) { return pair.first; }
+	EIter end(EPair pair) { return pair.second; }
+}
+
+std::ostream& operator<<(std::ostream& o, std::pair<Graph::vertex_descriptor, const Graph&> in){
+	return o << "(" << in.second[in.first].name << ": " << in.second[in.first].type << ")";
+}
+
+std::ostream& operator<<(std::ostream& o, std::pair<Graph::edge_descriptor, const Graph&> in){
+	return o << "(" << std::make_pair(source(in.first, in.second), in.second) << " -> " << std::make_pair(target(in.first, in.second), in.second) <<  ")";
+}
+
+std::ostream& operator<<(std::ostream& o, const Graph& g){
+	o << "vertices = [";
+	for(const auto& vertex : vertices(g))
+		o << std::make_pair(vertex, g) << " ";
+	o << "]" << std::endl;
+	o << "edges    = [";
+	for(const auto& edge : edges(g))
+		o << std::make_pair(edge, g) << " ";
+	return o  << "]";
+}
 
 int main(){
 	ifstream inGraph(graphMLFile);
 	Graph g;
 	dynamic_properties props;
+	props.property("name", get(&NodeInfo::name, g));
+	props.property("type", get(&NodeInfo::type, g));
 	try {
 		read_graphml(inGraph, g, props);
 		cout << "Parsing of GraphML file: " << graphMLFile << " successfull" << endl;
+		cout << g << endl;
 	}
 	catch(const bad_parallel_edge& e){
 		cerr << "Unsupported parallel edge on parsing GraphML file: " << graphMLFile << endl;
@@ -32,6 +70,7 @@ int main(){
 	}
 	catch(const parse_error& e){
 		cerr << "Parse Error on parsing GraphML file: " << graphMLFile << endl;
+		cerr << e.error << ": " << e.statement << endl;
 		cerr << e.what() << endl;
 	}
 	catch(const graph_exception& e){
---
+++ misc/stripY.xslt
@@ -0,0 +1,64 @@
+<xsl:stylesheet version="2.0" xmlns="http://graphml.graphdrawing.org/xmlns" 
+															xmlns:ns="http://graphml.graphdrawing.org/xmlns" 
+															xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+															xmlns:y="http://www.yworks.com/xml/graphml"
+>
+	<xsl:output method="xml" indent="yes"/>
+	<xsl:strip-space elements="*" />
+
+	<xsl:template name="genKeyType">
+		<xsl:element name="key">
+			<xsl:attribute name="for">node</xsl:attribute>
+			<xsl:attribute name="id">d0</xsl:attribute>
+			<xsl:attribute name="attr.name">type</xsl:attribute>
+			<xsl:attribute name="attr.type">string</xsl:attribute>
+		</xsl:element>
+	</xsl:template>
+
+	<xsl:template name="genKeyName">
+		<xsl:element name="key">
+			<xsl:attribute name="for">node</xsl:attribute>
+			<xsl:attribute name="id">d1</xsl:attribute>
+			<xsl:attribute name="attr.name">name</xsl:attribute>
+			<xsl:attribute name="attr.type">string</xsl:attribute>
+		</xsl:element>
+	</xsl:template>
+
+	<xsl:template match="ns:graphml">
+		<xsl:copy>
+  		<xsl:apply-templates select="@*"/>
+			<xsl:call-template name="genKeyType"/>
+			<xsl:call-template name="genKeyName"/>
+  		<xsl:apply-templates select="node()"/>
+ 		</xsl:copy>
+	</xsl:template>
+
+
+	<xsl:template match="y:NodeLabel">
+		<xsl:element name="data">
+			<xsl:attribute name="key">d1</xsl:attribute>
+			<xsl:apply-templates select="text()"/>
+		</xsl:element>
+	</xsl:template>
+
+	<xsl:template match="y:Shape">
+		<xsl:element name="data">
+			<xsl:attribute name="key">d0</xsl:attribute>
+		<xsl:value-of select="./@type"/>
+		</xsl:element>
+	</xsl:template>
+
+	<xsl:template match="y:*">
+		<xsl:apply-templates />
+	</xsl:template>
+	<xsl:template match="ns:key"/>
+	<xsl:template match="ns:data">
+		<xsl:apply-templates />
+	</xsl:template>
+
+	<xsl:template match="@*|node()|text()">
+		<xsl:copy>
+  		<xsl:apply-templates select="@*|node()"/>
+ 		</xsl:copy>
+	</xsl:template>
+</xsl:stylesheet>
