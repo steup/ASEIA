aa0db94abb7090c0495a6efcd0abbb7f78db9bbc
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-08-09 23:53:58 +0200

    BUGFIX: removed wrong saturation math
            - Fixed checkBound
            - refactored saturating unertainty

--- include/ValueElement.h
+++ include/ValueElement.h
@@ -8,111 +8,109 @@
 #include <initializer_list>
 #include <limits>
 #include <iterator>
+#include <cmath>
 
 namespace {
   template<typename T>
   inline T abs(T a){
-    if(a<0)
-      return -a;
-    else
-      return a;
+    return a<0?-a:a;
+  }
+
+  inline void satAdd(float& a, float b){
+    a+=b;
   }
   
-	inline float satAdd(float& a, float b){
-		a+=b;
-		//TODO: add epsilon estimation
-		return 0;
-	}
-
   template<typename T>
-  inline T satAdd(T& a, T b) {
-		if( !a  || !b || (a<0 && b>0) || (a>0 && b<0))
-			return a+=b;
-
-		if( a<0) {
-			T diff = std::numeric_limits<T>::min() - a;
-				if( diff >  b){
-					a = std::numeric_limits<T>::min();
-					return diff-b;
-				}
-		}else{
-    	T diff = std::numeric_limits<T>::max() - a;
-    	if( diff < b ) {
-      	a = std::numeric_limits<T>::max();
-      	return b-diff;
-			}
-		}
-    
-		a += b;
-    return 0;
+  inline void satAdd(T& a, T b) {
+		if( a> 0 && b > 0 && std::numeric_limits<T>::max() - a < b )
+      	a=std::numeric_limits<T>::max();
+		if( a< 0 && b < 0 && std::numeric_limits<T>::min() - a > b )
+      	a=std::numeric_limits<T>::min();
+    a+=b;
   }
 
   template<typename T>
-  inline T satSub(T& a, T b) {
-    T diff = std::numeric_limits<T>::max() + b;
-    if( diff > a ) {
-      a = std::numeric_limits<T>::lowest();
-      return diff-a;
-    } else {
-      a -= b;
-      return 0;
-    }
+  inline void satSub(T& a, T b){
+    satAdd(a, -b);
   }
 
   template<typename T1, typename T2>
   inline T1 modifyU(T1 u, T2 dummy){
-    return u;
+    return 0;
   }
   
 
   template<typename T>
   inline float modifyU(float u, T dummy){
-    satAdd(u, 1.0f);
-    return u;
+    return 1.0f;
   }
 
   template<typename T>
   inline double modifyU(double u, T dummy){
-    satAdd(u, 1.0);
-    return u;
+    return 1.0;
+  }
+  
+  template<>
+  inline float modifyU(float u, float dummy){
+    return 0.0f;
+  }
+  
+  template<>
+  inline double modifyU(double u, double dummy){
+    return 0.0;
   }
 
   template<>
   inline float modifyU(float u, double dummy){
-    return u;
+    return 0.0f;
   }
 
   template<>
   inline double modifyU(double u, float dummy){
-    satAdd(u, (double)std::numeric_limits<float>::epsilon());
-    return u;
+    return std::numeric_limits<float>::epsilon();
   }
 
 
-  template<typename T1, typename T2>
-  inline T2 checkBounds(T1& v, T1 u, T2 dummy) {
-    T1 T1min = std::numeric_limits<T1>::lowest();
-    T2 T2min = std::numeric_limits<T2>::lowest();
-    T2 T2max = std::numeric_limits<T2>::max();
-    T1 temp=0;
-    if(T1min < (T1)0 && T2min == (T2)0 && v<(T1)0){
-        temp = -v;
-        v = (T1)0;
-    }
-    if(T1min < (T1)0 && T2min < (T2)0 && v<(T1)T2min) {
-      v = T2min;
-      temp = T2min - v;
-    }
-    if(v>(T1)T2max){
-      v = T2max;
-      temp = T2max - v;
-    }
-    satAdd(u, temp);
+  template<typename From, typename To>
+  inline From checkBounds(From v, To& toV) {
+    To   toMin       = std::numeric_limits<To>::lowest();
+    To   toMax       = std::numeric_limits<To>::max();
+    bool sameSize    = std::is_integral<From>::value && std::is_integral<To>::value && sizeof(From) == sizeof(To);
+    bool toSign      = !std::is_signed<From>::value && std::is_signed<To>::value;
+    bool fromSign    = std::is_signed<From>::value && !std::is_signed<To>::value;
+    using common     = typename std::common_type<From, To>::type;
+    bool toCommon    = std::is_same<common, To>::value;
 
-    u=modifyU(u, dummy);
-    if(u<(T1)0 || u>(T1)std::numeric_limits<T2>::max())
-      return std::numeric_limits<T2>::max();
-    return u;
+    toV = v;
+    if(fromSign && v<0) {
+      toV = 0;
+      From fromU = -v;
+      satAdd(fromU, modifyU(From(), To()));
+      return fromU;
+    }
+
+    if(sameSize && toSign && (From)v > (From)toMax) {
+      toV = toMax;
+      From fromU = v-(From)toMax;
+      satAdd(fromU, modifyU(From(), To()));
+      return fromU;
+    }
+
+    if(!toCommon && (common)v>(common)toMax){
+      toV = toMax;
+      From fromU = v-toMax;
+      satAdd(fromU, modifyU(From(), To()));
+      return fromU;
+    }
+    
+    if(!toCommon && (common)v<(common)toMin){
+      toV = toMin;
+      From fromU = v-toMin;
+      satAdd(fromU, modifyU(From(), To()));
+      return fromU;
+    }
+
+    return modifyU(From(), To());
   }
 
   template<typename T>
@@ -266,14 +264,15 @@ class ValueElement<T, true>{
       auto iter = i.begin();
       mValue = *iter;
       mUncertainty = *std::next(iter);
-      mUncertainty=checkBounds(mValue, mUncertainty, T());
+      if (mUncertainty < 0)
+        mUncertainty = std::numeric_limits<T>::max();
     }
 
     template<typename T2>
     ValueElement(const ValueElement<T2, true>& data){
-      T2 v = data.value();
-      mUncertainty=checkBounds(v, data.uncertainty(), T());
-      mValue=(BaseType)(v);
+      T2 u = checkBounds(data.value(), this->mValue);
+      satAdd(u, data.uncertainty());
+      checkBounds(u, this->mUncertainty);
     }
 
     ValueElement& operator=(const ValueElement& data) = default;
@@ -285,25 +284,28 @@ class ValueElement<T, true>{
     T uncertainty() const{ return mUncertainty; }
     void uncertainty(const T& u){ mUncertainty = u; }
 
-    ValueElement operator+=(const ValueElement& a){
-      BaseType temp = satAdd(mValue, a.mValue);
-      if(temp)
-        satAdd(mUncertainty, temp);
+    ValueElement& operator+=(const ValueElement& a){
+      mValue+=a.mValue;
       satAdd(mUncertainty, a.mUncertainty);
-      T e = opError(mValue);
-      if(e)
-        satAdd(mUncertainty, e);
+      satAdd(mUncertainty, opError(mValue));
       return *this;
     }
 
+    ValueElement operator-() const{
+      ValueElement temp(*this);
+      if(std::is_signed<T>::value)
+        temp.mValue=-mValue;
+      else{
+        satAdd(temp.mUncertainty, mValue);
+        temp.mValue=(T)0;
+      }
+      return temp;
+    }
+
     ValueElement operator-=(const ValueElement& a){
-      BaseType temp = satSub(mValue, a.mValue);
-      if(temp)
-        satAdd(mUncertainty, temp);
+      mValue-=a.mValue;
       satAdd(mUncertainty, a.mUncertainty);
-      T e = opError(mValue);
-      if(e)
-        satAdd(mUncertainty, e);
+      satAdd(mUncertainty, opError(mValue));
       return *this;
     }
 
@@ -327,9 +329,7 @@ class ValueElement<T, true>{
       }
       mUncertainty = (max - min) / 2;
       mValue       = min + mUncertainty;
-      T e = opError(mValue);
-      if(e)
-        satAdd(mUncertainty, e);
+      satAdd(mUncertainty, opError(mValue));
       return *this;
     }
 
@@ -359,9 +359,7 @@ class ValueElement<T, true>{
       }
       mUncertainty = (max - min) / 2;
       mValue       = min + mUncertainty;
-      T e = opError(mValue);
-      if(e)
-        satAdd(mUncertainty, e);
+      satAdd(mUncertainty, opError(mValue));
       return *this;
     }
 
@@ -428,6 +426,7 @@ class ValueElement<T, true>{
 		}
 
     explicit operator BaseType() const { return mValue; }
+    explicit operator int() const { return (int)mValue; }
     explicit operator ValueElement<T, false>() const { return ValueElement(mValue); }
 
     constexpr static std::size_t size() noexcept {return sizeof(mUncertainty)+sizeof(mValue);}
