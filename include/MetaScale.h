07de6918d11bfcfe1ba9eb2110ef7f710daf38be
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-10-05 23:39:15 +0200

    Added MetaScale support as extension of ScaleType
            - operator * and / for MetaScales
            - normalization of results using boost::math::gcd
            - (de)serialiazation, output and initailization using ScaleType
            - example test showing initalization and operations

---
+++ include/MetaScale.h
@@ -0,0 +1,13 @@
+#include <ScaleType.h>
+
+class MetaScale : public ScaleType {
+  private:
+    void normalize();
+  public:
+    MetaScale() = default;
+    template<std::intmax_t num, std::intmax_t denom>
+    MetaScale(const std::ratio<num, denom> scale) : ScaleType(scale) {}
+
+    MetaScale operator*(const MetaScale& b) const;
+    MetaScale operator/(const MetaScale& b) const;
+};
