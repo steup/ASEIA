16a1c0809edc5bf3246a8280132da987e833a7b0
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-10-26 20:57:28 +0100

    MetaiAttribute: Modified scaling Attribute example

--- example/metaAttribute.cpp
+++ example/metaAttribute.cpp
@@ -12,10 +12,10 @@ int main() {
   MetaFactory::instance();
   position.value() = MetaFactory::instance().create({{0,0}, {1,1}, {2,2}}, id::type::Int16::value());
   position.unit()  = Meter();
-  position.scale() = ratio<1,1>();
+  position.scale() = ratio<1,10>();
   distance.value() = MetaFactory::instance().create({{10,1}, {5,1}, {0,1}}, id::type::Int16::value());
   distance.unit()  = Meter();
-  distance.scale() = ratio<1,100>();
+  distance.scale() = ratio<1,1>();
   cout << "Pos       : " << endl << "\t" << position << endl;
   cout << "DIstance  : " << endl << "\t" << distance << endl;
 	position += distance;
