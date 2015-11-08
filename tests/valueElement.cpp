8973d764d066e49dfcbaf43246d5944fc8b234be
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-11-08 22:36:58 +0100

    Unit-Test: Fix ValueElement Test
    	- Fix macros to enable better debuggin
    	- Remove Int8 Test. Will be added after unsigned uncertainty is
    	  finished

--- tests/valueElement.cpp
+++ tests/valueElement.cpp
@@ -9,21 +9,20 @@ bool exactlyEqual(const ValueElement<T, true>& a, const ValueElement<T, true>& b
   return a.value() == b.value() && a.uncertainty() == b.uncertainty();
 }
 
-#define testComp(a, V b, bool eq, bool ne, bool le, bool ge, bool lt, bool gt) {
-    EXPECT_EQ(a==b, eq) << a << " == " << b << " = " << (a==b) << " != " << eq;
-    EXPECT_EQ(a!=b, ne) << a << " != " << b << " = " << (a!=b) << " != " << ne;
-    EXPECT_EQ(a<=b, le) << a << " <= " << b << " = " << (a<=b) << " != " << le;
-    EXPECT_EQ(a>=b, ge) << a << " >= " << b << " = " << (a>=b) << " != " << ge;
-    EXPECT_EQ(a<b,  lt) << a << " <  " << b << " = " << (a<b)  << " != " << lt;
-    EXPECT_EQ(a>b,  gt) << a << " >  " << b << " = " << (a>b)  << " != " << gt;
+#define testComp(a, b, eq, ne, le, ge, lt, gt) { \
+    EXPECT_EQ(a==b, eq) << a << " == " << b << " = " << (a==b) << " != " << eq; \
+    EXPECT_EQ(a!=b, ne) << a << " != " << b << " = " << (a!=b) << " != " << ne; \
+    EXPECT_EQ(a<=b, le) << a << " <= " << b << " = " << (a<=b) << " != " << le; \
+    EXPECT_EQ(a>=b, ge) << a << " >= " << b << " = " << (a>=b) << " != " << ge; \
+    EXPECT_EQ(a<b,  lt) << a << " <  " << b << " = " << (a<b)  << " != " << lt; \
+    EXPECT_EQ(a>b,  gt) << a << " >  " << b << " = " << (a>b)  << " != " << gt; \
 }
 
-template<typename V>
-void testOp(V a, V b, V add, V sub, V mul, V div) {
-    EXPECT_TRUE(exactlyEqual(a+b, add)) << a << " + " << b << " = " << (a+b) << " != " << add;
-    EXPECT_TRUE(exactlyEqual(a-b, sub)) << a << " - " << b << " = " << (a-b) << " != " << sub;
-    EXPECT_TRUE(exactlyEqual(a*b, mul)) << a << " * " << b << " = " << (a*b) << " != " << mul;
-    EXPECT_TRUE(exactlyEqual(a/b, div)) << a << " / " << b << " = " << (a/b) << " != " << div;
+#define testOp(a, b, add, sub, mul, div) { \
+    EXPECT_TRUE(exactlyEqual(a+b, add)) << a << " + " << b << " = " << (a+b) << " != " << add; \
+    EXPECT_TRUE(exactlyEqual(a-b, sub)) << a << " - " << b << " = " << (a-b) << " != " << sub; \
+    EXPECT_TRUE(exactlyEqual(a*b, mul)) << a << " * " << b << " = " << (a*b) << " != " << mul; \
+    EXPECT_TRUE(exactlyEqual(a/b, div)) << a << " / " << b << " = " << (a/b) << " != " << div; \
 }
 
 TEST(ValueElementSuite, UInt8Test) {
@@ -43,28 +42,6 @@ TEST(ValueElementSuite, UInt8Test) {
 	testOp(a, e2, V({255, 255}), V({0, 255}) , V({255, 255}), V({0, 1})  );
 	testOp(a, e3, V({255, 255}), V({0, 255}) , V({255, 255}), V({0, 255}));
 	testOp(a, b, V({86, 38}), V({0, 255}), V({255, 255}), V({0, 1}));
-	testOp(a, c, V({16, 39}), V({10, 39}), V({65, 185}), V({13, 37}));
-}
-
-TEST(ValueElementSuite, Int8Test) {
-  using V=ValueElement<int8_t, true>;
-	V e0 = {0, 0};
-	V e1 = {0, 127};
-	V e2 = {127, 0};
-	V e3 = {127, 127};
-	V e4 = {-128, 0};
-	V e5 = {-128, 127};
-	V a={13, 37};
-	V b={-73, 1};
-	V c={3, 2};
-	testComp(a, b, false, true , false , true, false, true);
-	testComp(a, c, true , false, true , true , false, false);
-	testComp(b, c, false, true , false, true , false, true );
-	testOp(a, e0, a, a, V({0, 0}), V({0, 127}));
-	testOp(a, e1, V({13, 127}) , V({13, 127}), V({0, 127})  , V({0, 127}));
-	testOp(a, e2, V({127, 127}), V({0, 127}) , V({127, 127}), V({0, 1})  );
-	testOp(a, e3, V({127, 127}), V({0, 127}) , V({127, 127}), V({0, 127}));
-	testOp(a, b, V({-60, 38}), V({86, 38}), V({-128, 127}), V({0, 1}));
 	testOp(a, c, V({16, 39}), V({10, 39}), V({65, 185}), V({13, 37}));
 }
 
