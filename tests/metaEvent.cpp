db2e216b09cb5c0c3c402aab638d334877abcc4b
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-11-17 15:22:38 +0100

    Unit-Tests: Add MetaEvent EventType cast test

--- tests/metaEvent.cpp
+++ tests/metaEvent.cpp
@@ -16,7 +16,7 @@ using namespace std;
 			MetaEvent e;
 			MetaEventSuite(){
 				MetaAttribute position = posID;
-				position.value() = f.create({{0,1}, {1,1}, {2,1}});
+				position.value() = f.create({{{0,1}, {1,1}, {2,1}}});
 				position.unit()  = Meter();
 				position.scale() = ratio<1, 1000>();
         e.add(position);
@@ -25,12 +25,18 @@ using namespace std;
 
 	TEST_F(MetaEventSuite, findAttributeTest) {
 		MetaAttribute test = posID;
-		test.value() = f.create({{0,1}, {1,1}, {2,1}});
+		test.value() = f.create({{0, 1, 2}});
 		test.unit()  = Meter();
 		test.scale() = ratio<1, 1000>();
     const MetaAttribute* posPtr = e.attribute(posID);
     ASSERT_NE(posPtr, nullptr) << "Position attribute could not be retrieved";
     const MetaAttribute& pos = *posPtr;
 		EXPECT_EQ(pos, test) << "Position attribute not stored correctly";
+	}
+	
+	TEST_F(MetaEventSuite, typeTest) {
+		EventType eT;
+		eT.add(AttributeType(posID, ValueType(id::type::Double::value(), 1, 3, true), ratio<1,1000>(), Meter()));
+		EXPECT_EQ((EventType)e, eT) << "Inferred MetaEvent-Type is wrong";
 	}
 }}
