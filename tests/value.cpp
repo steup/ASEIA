41dc6db888c0125391944ebe771cda3eccc0e51d
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-11-19 18:34:00 +0100

    Unit-Test: Add vector comparision test case

--- tests/value.cpp
+++ tests/value.cpp
@@ -4,12 +4,13 @@ namespace tests {
 
 namespace valueSuite {
 
-TEST(ValueSuite, UInt8Test) {
+TEST(ValueSuite, uInt8Test) {
 	Value<uint8_t, 1, 3, true> v0={{{13, 37}, {1, 1}, {3, 7}}};
 	Value<uint8_t, 1, 3, true> v2={{{73, 1}, {3, 1}, {7, 1}}};
 	EXPECT_EQ(v0, v0) << "Equally assigned values are not equal";
 	EXPECT_NE(v0, v2) << "Unequally assigned values are equal";
 	EXPECT_FALSE(v0==v2) << "Unequally assigned values are equal";
+	EXPECT_FALSE((v0<v2).prod()) << "Vector comparision failed";
 }
 
 }}
