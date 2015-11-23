cdc0ee4b5f306186df5448c3be682569d0a17386
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-11-23 12:49:20 +0100

    MetaFactory: Add bool data type

--- src/MetaFactory.cpp
+++ src/MetaFactory.cpp
@@ -61,6 +61,7 @@ class Conversions {
 
 MetaFactoryImplementation::MetaFactoryImplementation() : 
   creators{
+    {id::type::Bool  ::value(), &MetaValueImplementation<bool    >::factoryCreate},
     {id::type::UInt8 ::value(), &MetaValueImplementation<uint8_t >::factoryCreate},
     {id::type::UInt16::value(), &MetaValueImplementation<uint16_t>::factoryCreate},
     {id::type::UInt32::value(), &MetaValueImplementation<uint32_t>::factoryCreate},
@@ -72,7 +73,8 @@ MetaFactoryImplementation::MetaFactoryImplementation() :
     {id::type::Float ::value(), &MetaValueImplementation<float   >::factoryCreate},
     {id::type::Double::value(), &MetaValueImplementation<double  >::factoryCreate},
   }{ 
-    Conversions<uint8_t,
+    Conversions<bool,
+                uint8_t,
                 uint16_t,
                 uint32_t,
                 uint64_t,
