98b74b97f7bbcd880ead62d5367d14c3af13caa5
Dirk Steindorf <dirk.steindorf@gmail.com>
2015-12-03 15:58:52 +0100

    removed Validity from angleEvent example

--- example/angleEvent.cpp
+++ example/angleEvent.cpp
@@ -10,9 +10,7 @@ struct AngleEventConfig : public BaseConfig
 {
   using PositionValueType    = Value<int16_t, 2>;
   using PublisherIDValueType = Value<uint16_t, 1, 1, false>;
-  using ValidityValueType    = Value<uint8_t, 1, 1, false>;
   using PositionScale        = std::ratio<1, 100>;
-  using ValidityScale        = std::ratio<1, 100 >;
     
 };
 
@@ -26,7 +24,6 @@ int main()
   e.attribute(Position()).value()    = {{{100, 20}}, {{200,20}}};
   e.attribute(Time()).value()        = {{{(unsigned long)std::time(nullptr), 1}}};
   e.attribute(PublisherID()).value() = {{{55}}};
-  e.attribute(Validity()).value()    = {{{80}}};
   e.attribute(Angle()).value()       = {{{314}}};
   
   std::cout << e;  
