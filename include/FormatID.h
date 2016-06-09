3696952705d1765dedd44d7715f8ef4820a2e1b8
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-06-10 00:34:25 +0200

    FormatID: working hash-based FormatID

--- include/FormatID.h
+++ include/FormatID.h
@@ -1,17 +1,21 @@
 #pragma once
 
-#include <cstdint.h>
 #include <Serializer.h>
 #include <DeSerializer.h>
 #include <Endianess.h>
 
+#include <cstdint>
+
+class EventType;
+
 class FormatID{
 	private:
-		uint32_t mHash;
+		uint32_t mHash = 0;
   public:
     FormatID() = default;
     FormatID(const EventType& eT);
     uint32_t value() const { return mHash; }
+    operator uint32_t() const { return mHash; }
     bool operator==(const FormatID& b) const { return mHash == b.mHash; }
     bool operator<(const FormatID& b) const { return mHash < b.mHash; }
     static constexpr std::size_t size() noexcept { return sizeof(mHash); }
@@ -21,12 +25,12 @@ class FormatID{
 
 template<typename PB>
 Serializer<PB>& operator<<(Serializer<PB>& s, const FormatID& id){
-  return s << htonl(id.value());
+  return s << hton(id.value());
 }
 
 template<typename PB>
 DeSerializer<PB>& operator>>(DeSerializer<PB>& d, FormatID& id){
   d >> id.mHash;
-  id.mHash=ntohl(id.mHash);
+  id.mHash=ntoh(id.mHash);
   return d;
 }
--- src/FormatID.cpp
+++ src/FormatID.cpp
@@ -1,8 +1,12 @@
 #include <FormatID.h>
 #include <EventType.h>
-#include <EventID.h>
+#include <Serializer.h>
+
+#include <MurmurHash2.h>
 
 FormatID::FormatID(const EventType&  eT) {
-	for(const AttributeType& aT : eT)
-		
+  uint8_t buffer[eT.size()];
+  Serializer<uint8_t*> s(buffer);
+  s << eT;
+  mHash = MurmurHash2(buffer, sizeof(buffer), 0);
 }
