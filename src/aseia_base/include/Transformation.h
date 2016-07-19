cc014a4ce85a63f640314673af9a60731717eb0f
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-07-19 17:48:45 +0200

    Fused typeCheck in Constructor of Transform
    	- typeCheck is removed
    	- added mValid variable and getter to Transformation for
    	  querying
    	- Benefit: Validity check can be included in transformation
    	  construction

--- src/aseia_base/include/Transformation.h
+++ src/aseia_base/include/Transformation.h
@@ -5,24 +5,29 @@
 #include <memory>
 
 class MetaEvent;
+class EventType;
 
 class Transformation {
   protected:
     Transformation() = default;
-
+		bool mValid = false;
   public:
+		virtual ~Transformation() = default;
     using EventTypes = std::list<const EventType*>;
-    using Events = std::list<const MetaEvents*>;
-    using TransID = uint32_t;
+    using Events = std::list<const MetaEvent*>;
     using TransPtr = std::unique_ptr<Transformation>;
 
-    template<typename T>
-    static create(const EventType& out, const EventTypes& in) {
-      return TransPtr(new T(out, in));
-    }
-
-    virtual bool typeCheck(const EventType& out, const EventTypes& in) const =0;
     virtual bool check(const Events& events) const =0;
     virtual MetaEvent operator()(const Events& events) =0;
-    virtual print(std::ostream& o) const = 0;
+    virtual void print(std::ostream& o) const = 0;
+		bool valid() const { return mValid; }
+    
+		template<typename T>
+    static TransPtr create(const EventType& out, const EventTypes& in) {
+     	TransPtr t = TransPtr(new T(out, in));
+			if (t->valid())
+				return t;
+			else
+				return TransPtr();
+    }
 };
