d54fa7fc57741c6f50b3e97d8c5a27d8cc02a4c7
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-08-26 19:06:38 +0200

    MetaEvent: add EventType based initialization

--- include/MetaEvent.h
+++ include/MetaEvent.h
@@ -29,6 +29,8 @@ class MetaEvent{
 		};
 
   public:
+		MetaEvent() = default;
+		MetaEvent(const EventType& eT);
 		MetaAttribute* attribute(ID id);
 		const MetaAttribute* attribute(ID id) const;
     bool add(const MetaAttribute&);
