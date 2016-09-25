8d6a6bad18840bf9553d270981606f6bae4fac80
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-09-25 22:53:29 +0200

    KnowledgeBase: Removed TransformGenerator
            - removed lazyness
            - migrated generation to KnowledgeBase
            - extended KnowledgeBase to track EventTypes

--- include/KnowledgeBase.h
+++ include/KnowledgeBase.h
@@ -1,63 +1,19 @@
 #pragma once
 
 #include <Transformation.h>
-#include <EventType.h>
-#include <AbstractRegistry.h>
 
-#include <list>
 #include <vector>
-#include <iterator>
 
-class TransformGeneratorImpl;
-
-class TransformGenerator {
-  public:
-    using TransList = std::list<const Transformation*>;
-    using EventTypes = AbstractRegistry<EventType>;
-  private:
-    using IterData = std::pair<const Transformation*, std::list<const EventType*>>;
-    using ImplData = std::pair<const Transformation*, std::vector<EventID>>;
-    TransformGeneratorImpl* mImpl;
-    const EventType& mOut;
-    const EventTypes& mIn;
-    ImplData mImplData;
-    EventTypes::const_local_iterator mType, mTypeEnd;
-    IterData next();
-  public:
-    TransformGenerator(const EventType& out, const EventTypes& in);
-    ~TransformGenerator();
-    class Iterator
-      : public std::iterator<
-        std::input_iterator_tag,
-        Transformation::TransPtr>
-    {
-      private:
-        TransformGenerator& mGen;
-        IterData mData;
-        Iterator(TransformGenerator& tG, IterData data)
-          : mGen(tG), mData(data) { }
-      public:
-        /* TODO:implement compound Transformations */
-        Transformation::TransPtr operator*() const {
-          return mData.first->create(mGen.mOut, mData.second);
-        }
-        Iterator& operator++() {
-          mData = mGen.next();
-          return *this;
-        }
-        bool operator==(const Iterator& b) const;
-        bool operator!=(const Iterator& b) const { return !(*this==b); }
-       friend class TransformGenerator;
-    };
-    Iterator begin() { return Iterator(*this, next()); }
-    Iterator end();
-  friend class TransformGeneratorImpl;
-};
+class EventType;
 
 class KnowledgeBase {
   public:
-    using EventTypes = Transformer::EventTypes;
+    using Transformations = std::vector<ConfiguredTransformation>;
+
     static void registerTransformation(const Transformation& trans);
     static void unregisterTransformation(const Transformation& trans);
+    static void registerEventType(const EventType& eT);
+    static void unregisterEventType(const EventType& eT);
+    //TODO: query functions
+    static Transformations findTransforms(const EventType& goal);
 };
-
--- src/KnowledgeBase.cpp
+++ src/KnowledgeBase.cpp
@@ -3,149 +3,136 @@
 
 #include <Singleton.h>
 
-#include <list>
+#include <vector>
 #include <algorithm>
 
-using EventTypes = KnowledgeBase::EventTypes;
-using EventIDs = Transformation::EventIDs;
+#include <iostream>
 
 using namespace std;
 
-class InvalidTransformation : public Transformation {
-  public:
-    InvalidTransformation() : Transformation(EventID::any, {}) {}
-    virtual bool check(const EventType& out, const EventTypes& in) const {
-      return false;
-    }
-    virtual TransPtr create(const EventType& out, const EventTypes& in) const {
-      return TransPtr();
-    }
-    virtual void print(std::ostream& o) const {
-      o << "invalid";
-    }
-};
-
-static InvalidTransformation invalidTrans;
-
-TransformGenerator::Iterator TransformGenerator::end() { 
-  return TransformGenerator::Iterator(*this, IterData(&invalidTrans, {}));
-}
-
 class KBImpl {
   public:
-    using GeneralTransformStorage = std::list<const Transformation*>;
+    using TransStorage = std::vector<const Transformation*>;
+    using TypeStorage  = AbstractRegistry<EventType>;
+    using Transformations = KnowledgeBase::Transformations;
+    using EventIDs = TypeStorage::EventIDs;
+    using EventTypes = Transformation::EventTypes;
+
   private:
-    GeneralTransformStorage mGeneralTrans;
+    TransStorage mTrans;
+    TypeStorage  mTypes;
+
+    void filter(vector<const Transformation*>& trans, EventID goal, const EventIDs& ids) const {
+      auto useless = [&goal, &ids](const Transformation* t){
+        const auto& tIn = t->in(goal);
+        return goal>=t->out();// || !includes(tIn.begin(), tIn.end(), ids.begin(), ids.end());
+      };
+      auto endIt = remove_if(trans.begin(), trans.end(), useless);
+      trans.erase(endIt, trans.end());
+    }
+
+    void addValidTypes(EventTypes& list, EventID id, const EventIDs& ids) const {
+      auto it = lower_bound(ids.begin(), ids.end(), id);
+      for(;it!=ids.end();it++)
+        if(id<=*it)
+          transform(mTypes.find(*it).first, mTypes.find(*it).second, back_inserter(list), [](const EventType& eT){ return &eT;});
+    }
+
+    vector<EventTypes> generateTypeLists(const EventIDs& in, const EventIDs& ids) const{
+      vector<EventTypes> result(in.size());
+      for(EventID id : in)
+        addValidTypes(result.back(), id, ids);
+      return result;
+    }
+
+    vector<EventTypes> generateCombinations(const vector<EventTypes>& typeLists) const {
+      vector<EventTypes> result;
+      if(typeLists.empty())
+        return result;
+      auto toList = [](const EventType* eT){ return EventTypes({eT});};
+      transform(typeLists[0].begin(), typeLists[0].end(), back_inserter(result), toList);
+      for(size_t i=1; i<typeLists.size(); i++) {
+        auto currStart = result.begin();
+        for(const EventType* b : typeLists[i]) {
+          auto currEnd = result.end();
+          copy(currStart, currEnd, back_inserter(result));
+          for(; currStart!=currEnd; currStart++){
+            currStart->push_back(b);
+          }
+        }
+      }
+      return result;
+    }
+
+    Transformations generate(vector<const Transformation*>& trans,
+                  const EventType& goal, const EventIDs& ids) const {
+      Transformations result;
+      for(const Transformation* t : trans) {
+        vector<EventTypes> usableEventTypes = generateTypeLists(t->in(goal), ids);
+        vector<EventTypes> typeCombinations = generateCombinations(usableEventTypes);
+        auto create = [t, &goal](const EventTypes& in){
+          return ConfiguredTransformation(*t, goal, in);
+        };
+        transform(typeCombinations.begin(), typeCombinations.end(), back_inserter(result), create);
+      }
+      return result;
+    }
   public:
-    using GeneralTransformIter = GeneralTransformStorage::const_iterator;
-    /*TODO: Implement registering of Transformation */
     void regTrans(const Transformation& trans) {
-      if(&trans==&invalidTrans)
+      auto comp = [&trans](const Transformation* t){
+        return *t == trans;
+      };
+      if(trans==Transformation::invalid)
+        return;
+      if(any_of(mTrans.begin(), mTrans.end(), comp))
         return;
       if(trans.out() == EventID::any)
-        mGeneralTrans.push_back(&trans);
+        mTrans.push_back(&trans);
     }
-    /*TODO: Implement unregistering of Transformation */
-    void unregTrans(const Transformation& trans) {
 
+    void unregTrans(const Transformation& trans) {
+      auto comp = [&trans](const Transformation* t){
+        return *t == trans;
+      };
+      auto it = find_if(mTrans.begin(), mTrans.end(), comp);
+      if(it == mTrans.end())
+        return;
+      iter_swap(it, mTrans.end()-1);
+      mTrans.erase(mTrans.end()-1);
     }
-    const GeneralTransformStorage& generalTrans() const { return mGeneralTrans; }
+
+    void regType(const EventType& eT) {
+      mTypes.registerType(eT, eT);
+    }
+
+    void unregType(const EventType& eT) {
+      mTypes.unregisterType(eT);
+    }
+
+    Transformations find(const EventType& goal) {
+      EventIDs ids = mTypes.ids(); //<< Get EventIDs currently registered
+      sort(ids.begin(), ids.end());//<< Sort EventIDs ascending
+
+      cout << "EventIDs: " << ids.size() << endl;
+      EventID out = (EventID)goal; //<< Get goal EventID
+      std::vector<const Transformation*> trans(mTrans); //<< Copy list of transformations
+      cout << "Trans: " << trans.size() << endl;
+
+      filter(trans, out, ids); //<< Remove all transformation not leading to the goal EventID or with unfullfilled dependancies
+
+      cout << "Filtered Trans: " << trans.size() << endl;
+      Transformations result = generate(trans, goal, ids); //<< Generate all configurations
+      cout << "Configs: " << result.size() << endl;
+      auto resultEnd = remove_if(result.begin(), result.end(),
+        [](const ConfiguredTransformation& t){ return !t.check();}
+      ); //<< Remove invalid configurations
+      result.erase(resultEnd, result.end());
+      cout << "Filtered Configs: " << result.size() << endl;
+      return result;
+    }
 };
 
 using KB = Singleton<KBImpl>;
-
-
-class TransformGeneratorImpl {
-  public:
-    using ImplData = TransformGenerator::ImplData;
-    using EventIDs = ImplData::second_type;
-    const EventID  mOut;
-    const EventIDs mIn;
-    TransformGeneratorImpl(EventID out, EventIDs in) : mOut(out), mIn(in) {}
-    virtual ~TransformGeneratorImpl() {}
-    virtual ImplData next() =0;
-};
-
-class GeneralTransformGenerator : public TransformGeneratorImpl {
-  private:
-    using TransIter = KB::GeneralTransformIter;
-    using EventIter = EventIDs::const_iterator;
-    const TransIter mTransEnd = KB::instance().generalTrans().end();
-    TransIter mTrans = KB::instance().generalTrans().begin();
-    EventIDs mCompatibleIn;
-    EventIter mEvent;
-  public:
-    GeneralTransformGenerator(EventID out, EventIDs in)
-      : TransformGeneratorImpl(out, in), mEvent(mCompatibleIn.begin())
-    {}
-
-    virtual ImplData next() {
-      if(mEvent == mCompatibleIn.end())
-        mTrans = find_if(mTrans, mTransEnd, [this](const Transformation* t){return t->out()>=mOut;});
-
-      if(mTrans == mTransEnd)
-        return ImplData(&invalidTrans, {});
-
-      if(mEvent == mCompatibleIn.end()) {
-        copy_if(mIn.begin(), mIn.end(), std::back_inserter(mCompatibleIn),
-          [this](EventID id){return mOut <= id;}
-        );
-        mEvent = mCompatibleIn.begin();
-      }
-
-      if(mEvent == mCompatibleIn.end())
-        return ImplData(&invalidTrans, {});
-
-      ImplData res(*mTrans, {*mEvent++});
-
-      if(mEvent == mCompatibleIn.end())
-        mTrans++;
-
-      return res;
-    }
-};
-
-TransformGenerator::TransformGenerator(const EventType& out, const EventTypes& in)
-  :  mImpl(nullptr), mOut(out), mIn(in)
-{
-  ImplData::second_type ids;
-  std::transform(in.begin(), in.end(), std::back_inserter(ids), [](const EventType& e){return (EventID)e;});
-  std::sort(ids.begin(), ids.end());
-  ids.erase(std::unique(ids.begin(), ids.end()), ids.end());
-  mImpl= new GeneralTransformGenerator(out, ids);
-}
-
-TransformGenerator::~TransformGenerator() {
-  if(mImpl)
-    delete mImpl;
-}
-
-TransformGenerator::IterData TransformGenerator::next() {
-  if(mImpl) {
-    IterData i;
-    do {
-      //TODO hack for singular transforms
-      if(i.first == nullptr || mType != mTypeEnd) {
-        mImplData = mImpl->next();
-        i.first = mImplData.first;
-        if(mImplData.second.empty())
-          continue;
-        auto range = mIn.find(mImplData.second.front());
-        mType = range.begin();
-        mTypeEnd = range.end();
-      }
-      i.first = mImplData.first;
-      i.second.push_back(mType++);
-    } while(i.first != &invalidTrans && !(i.first->check(mOut, i.second)));
-    return i;
-  }else
-    return IterData(&invalidTrans, {});
-}
-
-bool TransformGenerator::Iterator::operator==(const TransformGenerator::Iterator& b) const {
-  return mData.first == b.mData.first && std::equal(mData.second.begin(), mData.second.end(), b.mData.second.begin());
-}
-
 
 void KnowledgeBase::registerTransformation(const Transformation& trans) {
   KB::instance().regTrans(trans);
@@ -153,4 +140,16 @@ void KnowledgeBase::registerTransformation(const Transformation& trans) {
 
 void KnowledgeBase::unregisterTransformation(const Transformation& trans) {
   KB::instance().unregTrans(trans);
+}
+
+void KnowledgeBase::registerEventType(const EventType& trans) {
+  KB::instance().regType(trans);
+}
+
+void KnowledgeBase::unregisterEventType(const EventType& trans) {
+  KB::instance().unregType(trans);
+}
+
+KnowledgeBase::Transformations KnowledgeBase::findTransforms(const EventType& goal) {
+  return KB::instance().find(goal);
 }
