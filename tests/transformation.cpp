5df629345ed6af671628271ae9a345c298efa1e4
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-09-25 22:54:21 +0200

    Unit-test: Adapt Unit-test to new KB interface

--- tests/transformation.cpp
+++ tests/transformation.cpp
@@ -1,10 +1,10 @@
 #include <KnowledgeBase.h>
 #include <MetaFactory.h>
-#include <TypeRegistry.h>
 
 namespace test {
 
 using namespace ::id::attribute;
+using namespace ::id::type;
 
 using TransPtr    = Transformation::TransPtr;
 using Events      = Transformer::Events;
@@ -12,7 +12,6 @@ using EventTypes  = Transformer::EventTypes;
 
 struct TransformTestSuite : public ::testing::Test{
 	MetaFactory&   f  = MetaFactory::instance();
-	AbstractRegistry<EventType> t;
 	Events       in;
 	EventTypes   inT;
 	MetaEvent    out;
@@ -33,19 +32,45 @@ TEST_F(TransformTestSuite, scaleTransformBasicTest) {
   s.scale() = MetaScale();
   out.add(s);
   inET = (EventType)inE;
-	t.registerType(inET, inET);
+	KnowledgeBase::registerEventType(inET);
   outT = (EventType)out;
   in   = { &inE };
-  std::list<TransPtr> transList;
-  TransformGenerator g(outT, t);
-  std::move(g.begin(), g.end(), std::back_inserter(transList));
+  auto transList=KnowledgeBase::findTransforms(outT);
   ASSERT_GT(transList.size(), 0) << "No Transform found!";
   std::ostringstream os;
-  for(const TransPtr& ptr : transList)
-    ptr->print(os);
+  for(const ConfiguredTransformation& t : transList)
+    os << t;
   ASSERT_LT(transList.size(), 2) << "Too many Transforms found: " << os.str();
-	EXPECT_TRUE(transList.front()->check(in)) << "MetaValue is not supported by ScaleTransform";
-	MetaEvent temp = (*transList.front())(in);
+  TransPtr scaleT = transList.front().create();
+	EXPECT_TRUE(scaleT->check(in)) << "MetaValue is not supported by ScaleTransform";
+	MetaEvent temp = (*scaleT)(in);
+	EXPECT_EQ(temp, out) << "Events not transformed correctly";
+}
+
+TEST_F(TransformTestSuite, typeTransformBasicTest) {
+  MetaEvent inE;
+  EventType  inET;
+  MetaAttribute fs(Time::value());
+  fs.unit() = Second();
+  fs.value() = f.create({{{1234.5, 0}}}, Float::value());
+  inE.add(fs);
+  MetaAttribute us64 = fs;
+  us64.value() = f.create({{{1234, 1}}}, UInt64::value());
+  us64.scale() = MetaScale();
+  out.add(us64);
+  inET = (EventType)inE;
+	KnowledgeBase::registerEventType(inET);
+  outT = (EventType)out;
+  in   = { &inE };
+  auto transList = KnowledgeBase::findTransforms(outT);
+  ASSERT_GT(transList.size(), 0) << "No Transform found!";
+  std::ostringstream os;
+  for(const ConfiguredTransformation& t : transList)
+    os << t;
+  ASSERT_LT(transList.size(), 2) << "Too many Transforms found: " << os.str();
+  TransPtr typeT = transList.front().create();
+	EXPECT_TRUE(typeT->check(in)) << "MetaValue is not supported by TypeTransform";
+	MetaEvent temp = (*typeT)(in);
 	EXPECT_EQ(temp, out) << "Events not transformed correctly";
 }
 
