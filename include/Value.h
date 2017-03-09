steup/ASEIA

commit be65539103c203f0ac80a34433583c4d317b4e4d

Value: switched from Eigen to Wrapped Eigen Matrix

include/Value.h
@@ -1,20 +1,13 @@
 #pragma once
 
+#include <Eigen/Core>
+
 #include <ratio>
-#define EIGEN_MATRIX_PLUGIN <ValueBase.h>
+#include <cmath>
 
 #include <ValueElement.h>
 #include <ValueType.h>
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#ifdef __clang__ 
-  #pragma GCC diagnostic ignored "-Wdeprecated-register"
-#endif
-#include <Eigen/Core>
-#pragma GCC diagnostic pop
 
-#include <cmath>
 
 namespace Eigen {
 	template<typename T, bool U>
@@ -29,33 +22,261 @@ namespace Eigen {
 
 template<typename T, bool U>
 const ValueElement<T, U>& conj(const ValueElement<T, U>& x)  { return x; }
+
 template<typename T, bool U>
 const ValueElement<T, U>& real(const ValueElement<T, U>& x)  { return x; }
+
 template<typename T, bool U>
 ValueElement<T, U> imag(const ValueElement<T, U>&)    { return 0.; }
+
 template<typename T, bool U>
 ValueElement<T, U> abs(const ValueElement<T, U>&  x)  { return ValueElement<T, U>({{std::abs(x.value()), x.uncertainty()}}); }
-template<typename T, bool U>
-ValueElement<T, U> abs2(const ValueElement<T, U>& x)  { return x*x; } 
-template<typename T, bool U>
-ValueElement<T, U> ceil(const ValueElement<T, U>& x)  { return ValueElement<T, U>({{ceil(x.value()), x.uncertainty()}}); } 
-template<typename T, bool U>
-ValueElement<T, U> log(const ValueElement<T, U>& x)  { return ValueElement<T, U>({{log(x.value()), log(x.uncertainty())}}); } 
 
-template<typename T, int32_t rows, int32_t cols = 1, bool useUncertainty=true>
-using Value = Eigen::Matrix<ValueElement<T, useUncertainty>, rows, cols>;
+template<typename T, bool U>
+ValueElement<T, U> abs2(const ValueElement<T, U>& x)  { return x*x; }
+
+template<typename T, bool U>
+ValueElement<T, U> ceil(const ValueElement<T, U>& x)  { return ValueElement<T, U>({{ceil(x.value()), x.uncertainty()}}); }
+
+template<typename T, bool U>
+ValueElement<T, U> log(const ValueElement<T, U>& x)  { return ValueElement<T, U>({{log(x.value()), log(x.uncertainty())}}); }
+
+
+template<typename T, int32_t R, int32_t C = 1, bool U=true>
+class Value {
+  private:
+    Eigen::Matrix<ValueElement<T, U>, R, C> mData;
+  public:
+		using BaseType      = ValueElement<T, U>;
+		using RowInitType   = std::initializer_list<BaseType>;
+		using InitType      = std::initializer_list<RowInitType>;
+		using TypeID        = typename BaseType::TypeID;
+		using Bool          = Eigen::Matrix<ValueElement<bool, U>, R, C>;
+    using Iterator      = BaseType*;
+    using ConstIterator = const BaseType*;
+
+		Iterator begin() { return Iterator(this->data()); }
+
+		Iterator end()   { return Iterator(this->data()+this->cols()*this->rows()); }
+
+		ConstIterator begin() const { return ConstIterator(this->data()); }
+
+		ConstIterator end() const { return ConstIterator(this->data()+this->cols()*this->rows()); }
+
+    void resize(std::size_t rows, std::size_t cols=1) { mData.resize(rows, cols); }
+    void conservativeResize(std::size_t rows, std::size_t cols=1) { mData.conservativeResize(rows, cols); }
+
+    Value() = default;
+    Value(std::size_t rows, std::size_t cols) : mData(rows, cols) {};
+    Value(const Eigen::Matrix<ValueElement<T, U>, R, C>& copy) : mData(copy) {}
+
+		Value(InitType l) : mData(l.size(), l.begin()->size()){
+      unsigned int i=0;
+      for(const auto& row : l) {
+				unsigned int j=0;
+        for(const auto& elem : row)
+          mData(i, j++)=elem;
+        i++;
+			}
+    }
+
+    std::size_t rows() const { return R==-1?mData.rows():R; }
+    std::size_t cols() const { return C==-1?mData.cols():C; }
+
+    BaseType prod() const { return mData.prod(); }
+    BaseType sum() const { return mData.sum(); }
+    BaseType* data() { return mData.data(); }
+    const BaseType* data() const { return mData.data(); }
+
+    template<std::intmax_t num, std::intmax_t den>
+    Value operator*(std::ratio<num, den> r) {
+      Value res = *this;
+      res *= BaseType(r.num);
+      res /= BaseType(r.den);
+      return res;
+    }
+
+    template<std::intmax_t num, std::intmax_t den>
+    Value& operator*=(std::ratio<num, den> r) {
+      mData *= BaseType(r.num);
+      mData /= BaseType(r.den);
+      return *this;
+    }
+
+    template<std::intmax_t num, std::intmax_t den>
+    Value operator/(std::ratio<num, den> r) {
+      Value res = *this;
+      res *= BaseType(r.den);
+      res /= BaseType(r.num);
+      return res;
+    }
+
+    template<std::intmax_t num, std::intmax_t den>
+    Value& operator/=(std::ratio<num, den> r) {
+      mData *= BaseType(r.den);
+      mData /= BaseType(r.num);
+      return *this;
+    }
+
+    Value& operator+=(const Value& b) {
+      mData+=b.mData;
+      return *this;
+    }
+
+    Value& operator-=(const Value& b) {
+      mData+=b.mData;
+      return *this;
+    }
+
+    template<int32_t R2, int32_t C2>
+    Value& operator*=(const Value<T, R2, C2, U>& b) {
+      mData*=b.mData;
+      return *this;
+    }
+
+    Value& operator*=(const BaseType& s) {
+      mData*=s;
+      return *this;
+    }
+
+    Value& operator/=(const BaseType& s) {
+      mData/=s;
+      return *this;
+    }
+
+    BaseType& operator()(int row, int col=0) {
+      return mData(row, col);
+    }
+
+    Value operator-() const {
+      Value temp=*this;
+      temp.mData = -temp.mData;
+      return temp;
+    }
+
+    const BaseType& operator()(int row, int col=0) const {
+      return mData(row, col);
+    }
+
+		explicit operator ValueType() const {
+			return ValueType(TypeID::value(),
+											 mData.rows(), mData.cols(),
+											 U);
+		}
+
+		Bool operator<( const Value& b ) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) < b(i, j);
+			return res;
+    }
+
+		Bool operator>( const Value& b ) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) > b(i, j);
+			return res;
+    }
+
+		Bool operator<=(const Value& b) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) <= b(i, j);
+			return res;
+		}
+
+		Bool operator>=(const Value& b) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) >= b(i, j);
+			return res;
+		}
+
+		Bool operator==(const Value& b) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) == b(i, j);
+			return res;
+		}
+
+		Bool operator!=(const Value& b) const {
+			Bool res(mData.rows(), mData.cols());
+			for(unsigned int i=0;i<mData.rows();i++)
+				for(unsigned int j=0;j<mData.cols();j++)
+					res(i, j) = mData(i, j) != b(i, j);
+			return res;
+		}
+
+    Bool approxEqual(const Value& b) const {
+      if(rows() != b.rows() || cols() != b.cols())
+        return Bool::Zero(rows(), cols());
+      Bool res(rows(), cols());
+      for(unsigned int i=0;i<rows();i++)
+        for(unsigned int j=0;j<cols();j++)
+          res(i, j) = ::approxEqual(mData(i, j), b.mData(i, j));
+      return res;
+    }
+
+    template<typename VE>
+    Value<typename VE::VType, R, C, VE::U::value> cast() const {
+      return Value<typename VE::VType, R, C, VE::U::value>(mData.cast<VE>());
+    }
+
+		constexpr const bool hasUncertainty() const noexcept {return U;}
+
+    static constexpr const std::size_t staticSize() { return R * C * BaseType::size(); }
+    std::size_t dynamicSize() const { return mData.rows() * mData.cols() * BaseType::size(); }
+
+    template<typename TF, int32_t RF, int32_t CF, bool UF, int32_t C2F>
+    friend Value<TF, RF, C2F, UF> operator*(const Value<TF, RF, CF, UF>&, const Value<TF, CF, C2F, UF>&);
+
+};
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator+(const Value<T, R, C, U>& a, const Value<T, R, C, U>& b) {
+  Value<T, R, C, U> temp=a;
+  return temp+=b;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator-(const Value<T, R, C, U>& a, const Value<T, R, C, U>& b) {
+  Value<T, R, C, U> temp=a;
+  return temp-=b;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator*(const ValueElement<T, U>& a, const Value<T, R, C, U>& b) {
+  Value<T, R, C, U> temp=b;
+  return temp*=a;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator*(const Value<T, R, C, U>& a, const ValueElement<T, U>& b) {
+  return b.mData*a.mData;
+}
+
+template<typename T, int32_t R, int32_t C, bool U, int32_t C2>
+Value<T, R, C2, U> operator*(const Value<T, R, C, U>& a, const Value<T, C, C2, U>& b) {
+  return b.mData*a.mData;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator/(const Value<T, R, C, U>& a, const ValueElement<T, U>& b) {
+  Value<T, R, C, U> temp=a;
+  return a/=b;
+}
 
 template<typename T, int32_t rows, int32_t cols, bool u>
 auto approxEqual(const Value<T, rows, cols, u>& a, const Value<T, rows, cols, u>& b) -> typename Value<T, rows, cols, u>::Bool {
-  using Bool = typename Value<T, rows, cols, u>::Bool;
-  if(a.rows() != b.rows() || a.cols() != b.cols())
-    return Bool::Zero(a.rows(), b.rows());
-  Bool res(a.rows(), a.cols());
-  for(unsigned int i=0;i<a.rows();i++)
-    for(unsigned int j=0;j<a.cols();j++)
-      res(i, j) = approxEqual(a(i, j), b(i, j));
-  return res;
+  return a.approxEqual(b);
 }
+
+
 
 template<typename PB, typename T, int32_t rows, int32_t cols, bool u>
 Serializer<PB>& operator<<(Serializer<PB>& s, const Value<T,rows,cols,u>& value){

include/IO.h
@@ -150,3 +150,14 @@ inline std::ostream& operator<<(std::ostream& o, EventID eID) {
 inline std::ostream& operator<<(std::ostream& o, FormatID fID) {
   return o << fID.value();
 }
+
+template<typename T, int32_t R, int32_t C, bool U>
+std::ostream& operator<<(std::ostream& o, const Value<T, R, C, U>& v) {
+  for(std::size_t r = 0; r<v.rows(); r++) {
+    o << "(";
+    for(std::size_t c = 0; c<v.cols(); c++)
+      o << "\t" << v(r, c);
+    o << ")";
+  }
+  return o;
+}

include/ValueBase.h
@@ -1,141 +1,0 @@
-	class Iterator {
-			Scalar *ptr;
-			public:
-				Iterator() = default;
-				Iterator(Scalar *ptr) : ptr(ptr) {}
-				Scalar& operator*() { return *ptr; }
-				Iterator& operator++() { ++ptr; return *this;}
-				Iterator  operator++(int) { Iterator temp(*this); ptr++; return temp;}
-				bool operator==(const Iterator& b) const { return ptr == b.ptr; }
-				bool operator!=(const Iterator& b) const { return ptr != b.ptr; }
-		};
-
-		class ConstIterator{
-			const Scalar *ptr;
-			public:
-				ConstIterator() = default;
-				ConstIterator(const Scalar* ptr) : ptr(ptr) {}
-				const Scalar& operator*() const { return *ptr; }
-				ConstIterator& operator++() { ++ptr; return *this;}
-				ConstIterator  operator++(int) { ConstIterator temp(*this); ptr++; return temp;}
-				bool operator==(const ConstIterator& b) const { return ptr == b.ptr; }
-				bool operator!=(const ConstIterator& b) const { return ptr != b.ptr; }
-		};
-
-		Iterator begin() { return Iterator(this->data()); }
-
-		Iterator end()   { return Iterator(this->data()+this->cols()*this->rows()); }
-
-		ConstIterator begin() const { return ConstIterator(this->data()); }
-
-		ConstIterator end() const { return ConstIterator(this->data()+this->cols()*this->rows()); }
-	
-		using RowInitType = std::initializer_list<Scalar>;
-		using InitType    = std::initializer_list<RowInitType>;
-		using BaseType    = Scalar;
-		using U           = typename BaseType::U;
-		using TypeID      = typename BaseType::TypeID;
-		using Bool        = Matrix<ValueElement<bool, U::value>, RowsAtCompileTime, ColsAtCompileTime>;
-    using Base::operator*;
-    using Base::operator*=;
-    using Base::operator/=;
-    using Base::operator/;
-
-		Matrix(InitType l) : Matrix(l.size(), l.begin()->size()){
-      unsigned int i=0;
-      for(const auto& row : l) {
-				unsigned int j=0;
-      	for(const auto& elem : row)
-        	this->operator()(i, j++)=elem;
-				i++;
-			}
-    }
-
-    template<std::intmax_t num, std::intmax_t den>
-    Matrix operator*(std::ratio<num, den> r) {
-      Matrix res = *this;
-      res *= Scalar(r.num);
-      res /= Scalar(r.den);
-      return res;
-    }
-    
-    template<std::intmax_t num, std::intmax_t den>
-    Matrix& operator*=(std::ratio<num, den> r) {
-      *this *= Scalar(r.num);
-      *this /= Scalar(r.den);
-      return *this;
-    }
-    
-    template<std::intmax_t num, std::intmax_t den>
-    Matrix operator/(std::ratio<num, den> r) {
-      Matrix res = *this;
-      res *= Scalar(r.den);
-      res /= Scalar(r.num);
-      return res;
-    }
-    
-    template<std::intmax_t num, std::intmax_t den>
-    Matrix& operator/=(std::ratio<num, den> r) {
-      *this *= Scalar(r.den);
-      *this /= Scalar(r.num);
-      return *this;
-    }
-
-		explicit operator ValueType() const {
-			return ValueType(TypeID::value(), 
-											 this->rows(), this->cols(), 
-											 U::value);
-		}
-
-		Bool operator<( const Matrix& b ) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) < b(i, j);
-			return res;
-    }
-
-		Bool operator>( const Matrix& b ) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) > b(i, j);
-			return res;
-    }
-
-		Bool operator<=(const Matrix& b) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) <= b(i, j);
-			return res;
-		}
-
-		Bool operator>=(const Matrix& b) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) >= b(i, j);
-			return res;
-		}
-
-		Bool operator==(const Matrix& b) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) == b(i, j);
-			return res;
-		}
-
-		Bool operator!=(const Matrix& b) const {
-			Bool res(this->rows(), this->cols());
-			for(unsigned int i=0;i<this->rows();i++)
-				for(unsigned int j=0;j<this->cols();j++)
-					res(i, j) = (*this)(i, j) != b(i, j);
-			return res;
-		}
-
-		constexpr const bool hasUncertainty() const noexcept {return U::value;}
-
-    static constexpr const std::size_t staticSize() { return RowsAtCompileTime * ColsAtCompileTime * BaseType::size(); }
-    std::size_t dynamicSize() const { return this->rows() * this->cols() * BaseType::size(); }

commit fdd8afa5785b8ee5b25ad84e1a368fb322e08093

Value: fix various bugs introduced by be655391

include/Value.h
@@ -51,6 +51,7 @@ class Value {
 		using RowInitType   = std::initializer_list<BaseType>;
 		using InitType      = std::initializer_list<RowInitType>;
 		using TypeID        = typename BaseType::TypeID;
+    using DataType      = Eigen::Matrix<BaseType, R, C>;
 		using Bool          = Eigen::Matrix<ValueElement<bool, U>, R, C>;
     using Iterator      = BaseType*;
     using ConstIterator = const BaseType*;
@@ -79,6 +80,9 @@ class Value {
         i++;
 			}
     }
+    
+    static Value Ones() { return Value(DataType::Ones()); }
+    static Value Zeros() { return Value(DataType::Zeros()); }
 
     std::size_t rows() const { return R==-1?mData.rows():R; }
     std::size_t cols() const { return C==-1?mData.cols():C; }
@@ -104,7 +108,7 @@ class Value {
     }
 
     template<std::intmax_t num, std::intmax_t den>
-    Value operator/(std::ratio<num, den> r) {
+    Value operator/(std::ratio<num, den> r) const {
       Value res = *this;
       res *= BaseType(r.den);
       res /= BaseType(r.num);
@@ -159,9 +163,9 @@ class Value {
     }
 
 		explicit operator ValueType() const {
-			return ValueType(TypeID::value(),
-											 mData.rows(), mData.cols(),
-											 U);
+			return ValueType(BaseType::TypeID::value(),
+											 rows(), cols(),
+											 hasUncertainty());
 		}
 
 		Bool operator<( const Value& b ) const {
@@ -227,7 +231,7 @@ class Value {
       return Value<typename VE::VType, R, C, VE::U::value>(mData.cast<VE>());
     }
 
-		constexpr const bool hasUncertainty() const noexcept {return U;}
+		constexpr const bool hasUncertainty() const noexcept {return BaseType().hasUncertainty();}
 
     static constexpr const std::size_t staticSize() { return R * C * BaseType::size(); }
     std::size_t dynamicSize() const { return mData.rows() * mData.cols() * BaseType::size(); }
@@ -250,6 +254,11 @@ Value<T, R, C, U> operator-(const Value<T, R, C, U>& a, const Value<T, R, C, U>&
 }
 
 template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator*(T a, const Value<T, R, C, U>& b) {
+  return b*a;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
 Value<T, R, C, U> operator*(const ValueElement<T, U>& a, const Value<T, R, C, U>& b) {
   Value<T, R, C, U> temp=b;
   return temp*=a;
@@ -257,7 +266,12 @@ Value<T, R, C, U> operator*(const ValueElement<T, U>& a, const Value<T, R, C, U>
 
 template<typename T, int32_t R, int32_t C, bool U>
 Value<T, R, C, U> operator*(const Value<T, R, C, U>& a, const ValueElement<T, U>& b) {
-  return b.mData*a.mData;
+  return b*a;
+}
+
+template<typename T, int32_t R, int32_t C, bool U>
+Value<T, R, C, U> operator*(const Value<T, R, C, U>& a, T b) {
+  return ValueElement<T,U>(b)*a;
 }
 
 template<typename T, int32_t R, int32_t C, bool U, int32_t C2>