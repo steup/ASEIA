5f90bbf8f2955abf5ff60d10cb23c669a0ac51fe
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-05-15 15:24:31 +0200

    Unit-Tests: Add EventType compare test case

--- tests/event.cpp
+++ tests/event.cpp
@@ -62,4 +62,56 @@ TEST(EventTestSuite, idTest) {
 	EXPECT_FALSE(C<=B);
 	EXPECT_FALSE(C==B);
 }
+
+TEST(EventTestSuite, typeTest) {
+	using EventA=BaseEvent<>;
+	using Distance = Attribute<Distance, Value<float, 1>, Meter>;
+	using Angle = Attribute<Angle, Value<float, 1>, Radian>;
+	using EventB=EventA::append<Distance>::type;
+	using EventC=EventA::append<Angle>::type;
+	EventA a;
+	EventB b;
+	EventC c;
+  EventType A=(EventType)a;
+  EventType B=(EventType)b;
+  EventType C=(EventType)c;
+	EXPECT_TRUE(A<=B);
+	EXPECT_TRUE(A<B);
+	EXPECT_TRUE(A!=B);
+	EXPECT_FALSE(A>B);
+	EXPECT_FALSE(A>=B);
+	EXPECT_FALSE(A==B);
+	EXPECT_TRUE(B>=A);
+	EXPECT_TRUE(B>A);
+	EXPECT_TRUE(B!=A);
+	EXPECT_FALSE(B<A);
+	EXPECT_FALSE(B<=A);
+	EXPECT_FALSE(B==A);
+	EXPECT_TRUE(A<=C);
+	EXPECT_TRUE(A<C);
+	EXPECT_TRUE(A!=C);
+	EXPECT_FALSE(A>C);
+	EXPECT_FALSE(A>=C);
+	EXPECT_FALSE(A==C);
+	EXPECT_TRUE(C>=A);
+	EXPECT_TRUE(C>A);
+	EXPECT_TRUE(C!=A);
+	EXPECT_FALSE(C<A);
+	EXPECT_FALSE(C<=A);
+	EXPECT_FALSE(C==A);
+	EXPECT_FALSE(B<=C);
+	EXPECT_FALSE(B<C);
+	EXPECT_TRUE(B!=C);
+	EXPECT_FALSE(B>C);
+	EXPECT_FALSE(B>=C);
+	EXPECT_FALSE(B==C);
+	EXPECT_FALSE(C>=B);
+	EXPECT_FALSE(C>B);
+	EXPECT_TRUE(C!=B);
+	EXPECT_FALSE(C<B);
+	EXPECT_FALSE(C<=B);
+	EXPECT_FALSE(C==B);
+}
+
+
 }}
